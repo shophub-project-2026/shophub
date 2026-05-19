cfaafee3b398b0e99549bd716f4e8761aa06c93a
MilanJovkic <milanjovkic@gmail.com>
2026-05-19 21:35:02 +0200

    feat(config): add database and JWT env vars to Config struct

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,11 +3,15 @@ package config
 import (
 	"os"
 	"strconv"
+
+	"github.com/shophub-project-2026/shophub/internal/db"
 )
 
 type Config struct {
-	HTTPAddr string
-	HTTPPort int
+	HTTPAddr  string
+	HTTPPort  int
+	DB        db.Config
+	JWTSecret string
 }
 
 func Load() Config {
@@ -15,9 +19,21 @@ func Load() Config {
 	if err != nil {
 		port = 8080
 	}
+	dbPort, err := strconv.Atoi(getEnv("SHOPHUB_DB_PORT", "5432"))
+	if err != nil {
+		dbPort = 5432
+	}
 	return Config{
-		HTTPAddr: getEnv("SHOPHUB_HTTP_ADDR", "0.0.0.0"),
-		HTTPPort: port,
+		HTTPAddr:  getEnv("SHOPHUB_HTTP_ADDR", "0.0.0.0"),
+		HTTPPort:  port,
+		JWTSecret: getEnv("SHOPHUB_JWT_SECRET", "change-me-in-production"),
+		DB: db.Config{
+			Host:     getEnv("SHOPHUB_DB_HOST", "localhost"),
+			Port:     dbPort,
+			Name:     getEnv("SHOPHUB_DB_NAME", "shophub_db"),
+			User:     getEnv("SHOPHUB_DB_USER", "shophub_user"),
+			Password: getEnv("SHOPHUB_DB_PASSWORD", "shophub_password"),
+		},
 	}
 }
 
